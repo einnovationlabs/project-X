d5e9c020606fe3717c64c01d933106840ec2fa49
Jephthah Mensah <jephthahmensah@dhcp-vl2041-32662.eduroam.cornell.edu>
2023-07-25 01:00:49 -0400

    added endpoints for dataset and org

--- main/user/urls.py
+++ main/user/urls.py
@@ -3,9 +3,21 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+
     path('users/get/<int:user_id>/', views.get_user, name='get_user'),
     path('users/', views.create_user, name='create_user'),
     path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),
     path('users/update/<int:user_id>/', views.update_user, name='update_user'),
+
+    path('orgs/', views.create_org, name = "create_org"),
+    path('orgs/get/<int:org_id>/', views.get_org, name = "get_org"),
+    path('orgs/delete/<int:org_id>/', views.delete_org, name = "delete_org"),
+    path('orgs/update/<int:org_id>/', views.update_org, name = "update_org"),
+
+
+    path('orgs/', views.create_dataset, name = "create_dataset"),
+    path('orgs/get/<int:dataset_id>/', views.get_dataset, name = "get_dataset"),
+    path('orgs/delete/<int:dataset_id>/', views.delete_dataset, name = "delete_dataset"),
+    path('orgs/update/<int:dataset_id>/', views.update_dataset, name = "update_dataset"),
 
 ]
--- main/user/views.py
+++ main/user/views.py
@@ -13,7 +13,9 @@ def home(request):
 
 @csrf_exempt
 def create_user(request):
-
+    """
+    Endpoint to create user
+    """
     data = json.loads(request.body)
 
     # perform validation
@@ -24,12 +26,18 @@ def create_user(request):
 
 @csrf_exempt
 def delete_user(request, user_id):
+    """
+    Endpoint to delete user by id
+    """
     user = user_dao.delete_user(user_id)
     return JsonResponse(user.serialize())
 
 
 @csrf_exempt
 def update_user(request, user_id):
+    """
+    Endpoint to update user by id
+    """
     data = json.loads(request.body)
     user = user_dao.update_user(user_id, data)
     return JsonResponse(user.serialize())
@@ -37,6 +45,56 @@ def update_user(request, user_id):
 
 @csrf_exempt
 def get_user(request, user_id):
+    """
+    Endpoint to get user by id
+    """
     user = User.objects.get(id = user_id)
     return JsonResponse(user.serialize())
 
+@csrf_exempt
+def create_org(request):
+    """
+    Endpoint to create organization
+    """
+
+@csrf_exempt
+def delete_org(request, org_id):
+    """
+    Endpoint to delete organization by id
+    """
+
+@csrf_exempt
+def update_org(reques, org_id):
+    """
+    Endpoint to update organization by id
+    """
+
+@csrf_exempt
+def get_org(request, org_id):
+    """
+    Endpoint to get organization by id
+    """
+
+@csrf_exempt
+def create_dataset(request):
+    """
+    Endpoint to create dataset
+    """
+
+@csrf_exempt
+def delete_dataset(request, dataset_id):
+    """
+    Endpoint to delete dataset by id
+    """
+
+@csrf_exempt
+def update_dataset(reques, dataset_id):
+    """
+    Endpoint to update dataset by id
+    """
+
+@csrf_exempt
+def get_dataset(request, dataset_id):
+    """
+    Endpoint to get dataset by id
+    """
