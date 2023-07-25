5280a4c43e2c475f927000a3e7ae3d94d20fe301
Jephthah Mensah <jephthahmensah@dhcp-vl2041-32662.eduroam.cornell.edu>
2023-07-25 09:51:47 -0400

    added crud to daos

--- main/user/dataset_dao.py
+++ main/user/dataset_dao.py
@@ -3,3 +3,47 @@ DAO (Data Access Object) file
 
 Helper file containing functions for accessing data in our database
 """
+from user.models import Dataset
+
+def create_dataset(dataset_data):
+
+    dataset = dataset(firstname = dataset_data.get('firstname'), 
+            lastname = dataset_data.get('lastname'), 
+            datasetname = dataset_data.get('datasetname'),
+            password = dataset_data.get('password'),
+            about = dataset_data.get('about'),
+            email = dataset_data.get('email'),
+            phone_number = dataset_data.get('phone_number'),
+            profile_pic = dataset_data.get('profile_pic')
+            )
+    
+    
+    dataset.save()
+
+    return dataset
+
+
+
+def update_dataset(dataset_id, dataset_data):
+
+    dataset = dataset.objects.get(id = dataset_id)
+
+    dataset.firstname = dataset_data.get('firstname')
+    dataset.lastname = dataset_data.get('lastname')
+    dataset.datasetname = dataset_data.get('datasetname')
+    dataset.password = dataset_data.get('password')
+    dataset.about = dataset_data.get('about')
+    dataset.email = dataset_data.get('email')
+    dataset.phone_number = dataset_data.get('phone_number')
+    dataset.profile_pic = dataset_data.get('profile_pic')
+
+    dataset.save()
+
+    return dataset
+
+
+def delete_dataset(dataset_id):
+    dataset = dataset.objects.get(id = dataset_id)
+    dataset.is_active = False
+    dataset.save()
+    return dataset
--- main/user/organization_dao.py
+++ main/user/organization_dao.py
@@ -3,3 +3,47 @@ DAO (Data Access Object) file
 
 Helper file containing functions for accessing data in our database
 """
+from user.models import Organization
+
+def create_org(org_data):
+
+    org = org(firstname = org_data.get('firstname'), 
+            lastname = org_data.get('lastname'), 
+            orgname = org_data.get('orgname'),
+            password = org_data.get('password'),
+            about = org_data.get('about'),
+            email = org_data.get('email'),
+            phone_number = org_data.get('phone_number'),
+            profile_pic = org_data.get('profile_pic')
+            )
+    
+    
+    org.save()
+
+    return org
+
+
+
+def update_org(org_id, org_data):
+
+    org = org.objects.get(id = org_id)
+
+    org.firstname = org_data.get('firstname')
+    org.lastname = org_data.get('lastname')
+    org.orgname = org_data.get('orgname')
+    org.password = org_data.get('password')
+    org.about = org_data.get('about')
+    org.email = org_data.get('email')
+    org.phone_number = org_data.get('phone_number')
+    org.profile_pic = org_data.get('profile_pic')
+
+    org.save()
+
+    return org
+
+
+def delete_org(org_id):
+    org = org.objects.get(id = org_id)
+    org.is_active = False
+    org.save()
+    return org
